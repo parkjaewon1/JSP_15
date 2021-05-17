9b99d07f6d4b5a7dbd7347e3ba9573d49d40d18b
parkjaewon1 <48577510+parkjaewon1@users.noreply.github.com>
2021-05-17 10:22:56 +0900

    2021-05-17

--- ch16/src/main/java/ch16/EmpDao.java
+++ ch16/src/main/java/ch16/EmpDao.java
@@ -6,11 +6,13 @@ import java.util.List;
 import javax.naming.*;
 import javax.sql.DataSource;
 public class EmpDao {
+	// singleton
 	private static EmpDao instance = new EmpDao();
 	private EmpDao() {}
 	public static EmpDao getInstance() {
 		return instance;
 	}
+	
 	private Connection getConnection() {
 		Connection conn = null;
 		try {
@@ -47,6 +49,45 @@ public class EmpDao {
 				emp.setSal(rs.getInt("sal"));
 				emp.setComm(rs.getInt("comm"));
 				emp.setDeptno(rs.getInt("deptno"));
+
+				list.add(emp);
+			}
+		}catch (Exception e) {
+			System.out.println(e.getMessage());
+		}finally {
+			try {
+				if (rs != null) rs.close();
+				if (pstmt != null) pstmt.close();
+				if (conn != null) conn.close();
+			}catch (Exception e) {		}
+		}
+		return list;
+	}
+	public List<Emp2> list2(int deptno) {
+		List<Emp2> list = new ArrayList<Emp2>();
+		ResultSet rs = null;
+		PreparedStatement pstmt = null;
+		Connection conn = getConnection();
+		String sql = "select * from emp";
+		try {
+			if (deptno == 0) {
+				pstmt = conn.prepareStatement(sql);
+			} else {
+				sql += " where deptno=?";
+				pstmt = conn.prepareStatement(sql);
+				pstmt.setInt(1, deptno);
+			}
+			rs = pstmt.executeQuery();
+			while(rs.next()) {
+				Emp2 emp = new Emp2();
+				emp.setEmpno(rs.getString("empno"));
+				emp.setEname(rs.getString("ename"));
+				emp.setJob(rs.getString("job"));
+				emp.setMgr(rs.getString("mgr"));
+				emp.setHiredate(rs.getString("hiredate"));
+				emp.setSal(rs.getString("sal"));
+				emp.setComm(rs.getString("comm"));
+				emp.setDeptno(rs.getString("deptno"));
 
 				list.add(emp);
 			}
